4755fc3294ef4250c3e8c06e3cb3e729db83a725
FanisNR <fankirbi@mail.ru>
2021-06-03 10:28:25 +0300

    3.3-13.1-CSS3 js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 
-    $(".owl-carousel").owlCarousel({
+      /*  бургер++++++++++++++========================*/
+   $(document).ready(function() {
+       $(".menu__btn").click(function(event) {
+           $(".menu__btn,.wrapper").toggleClass("active");
+           $(".cinema").toggleClass("lock");
+       });
+   });
+
+
+   /*  карусель++++++++++++++========================*/
+   
+   $(".owl-carousel").owlCarousel({
         loop: true,
         nav: true,
         dots: false,
 
         center:true,
-
-
     });
